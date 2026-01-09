237c29f436fd7f79df648aa94c8491eeaba0cf0e
gabrieldevreis <gabrielreisdev@gmail.com>
2026-01-08 22:10:43 -0300

    refactor(igreja): remove módulo antigo de igreja

--- src/igreja/dto/create-igreja.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString, MinLength  } from "class-validator";
-import { UUID } from "crypto";
-
-export class CreateIgrejaDto {
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(3)
-    name: string;
-}
